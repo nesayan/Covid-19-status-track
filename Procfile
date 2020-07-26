be8eb4f5d891112709f80fa91ef4541a8aa913a1
nesayan <65268993+nesayan@users.noreply.github.com>
2020-07-26 10:39:12 +0530

    Initial Upload of West Bengal COVID-19 track
    Initial upload of West Bengal COVID-19 Track project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ServerCOVIDWestBengalTracker:app
---
+++ ServerCOVIDWestBengalTracker.py
@@ -0,0 +1,26 @@
+from COVIDWestBengalTracker import state, stateIndex, stateValues
+from flask import Flask
+from twilio.twiml.messaging_response import MessagingResponse
+  
+
+app = Flask(__name__) 
+
+@app.route('/') 
+def test():
+    resp = MessagingResponse()
+    resp.message("Working!")
+    return str(resp)
+
+@app.route('/track', methods= ['POST']) 
+def sendCOVIDStatus(): 
+    stateName = state
+    index = stateIndex
+    values = stateValues
+    resp = MessagingResponse()
+    resp.message(f"State: {stateName}\n{index[0]}: {values[0]}\n{index[1]}: {values[1]}\n{index[2]}: {values[2]}\n{index[3]}: {values[3]}")
+    return str(resp)
+
+# main driver function 
+if __name__ == '__main__': 
+
+    app.run(debug= True)
